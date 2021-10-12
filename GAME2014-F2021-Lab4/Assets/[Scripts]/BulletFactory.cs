0c6bec3e9bdc74e8be3e2503789aa9521c4ec317
Vincent329 <vincee.ho@gmail.com>
2021-10-12 14:22:46 -0400

    [ADD] - update README, bullets now spawn through the bullet factory

--- GAME2014-F2021-Lab4/Assets/[Scripts]/BulletFactory.cs
+++ GAME2014-F2021-Lab4/Assets/[Scripts]/BulletFactory.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class BulletFactory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Bullet Types")]
+    public GameObject enemyBullet;
+        public GameObject playerBullet;
 
-    // Update is called once per frame
-    void Update()
+    public GameObject createBullet(BulletType type = BulletType.ENEMY)
     {
-        
+        GameObject temp_bullet = null;
+        switch (type)
+        {
+            case BulletType.ENEMY:
+                temp_bullet = Instantiate(enemyBullet);
+                break;
+            case BulletType.PLAYER:
+                temp_bullet = Instantiate(playerBullet);
+
+                break;
+        }
+        temp_bullet.transform.parent = transform;
+        temp_bullet.SetActive(false);
+
+        return temp_bullet;
     }
 }
